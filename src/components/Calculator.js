f81996acb1f1ac16e078ca668db3d29d8e5bd96d
Damola Adediran <tylher123@gmail.com>
2022-04-19 14:33:56 +0100

    create calculator UI

---
+++ src/components/Calculator.js
@@ -0,0 +1,43 @@
+import { Component } from "react";
+
+export default class Calculator  extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <table>
+                <tr>
+                    <td>AC</td>
+                    <td>+/-</td>
+                    <td>%</td>
+                    <td class="right">&#247;</td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+                    <td class="right">&#215;</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td class="right">&#8722;</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td class="right">&#43;</td>
+                </tr>
+                <tr>
+                    <td colspan="2">0</td>
+                    <td>.</td>
+                    <td class="right">&#43;</td>
+                </tr>
+            </table>
+        )
+    }
+}
