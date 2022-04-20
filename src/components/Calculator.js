f9b75586f325ada2787b3e1cf4edfd07110f2642
Damola Adediran <tylher123@gmail.com>
2022-04-20 10:05:18 +0100

    Refactor code for calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,43 @@
 import { Component } from 'react';
 import './Calculator.css';
 
+const content = [
+  ['AC', '+/-', '%', '÷'],
+  ['7', '8', '9', '×'],
+  ['4', '5', '6', '-'],
+  ['1', '2', '3', '+'],
+  ['0', '.', '='],
+];
+
 // eslint-disable-next-line react/prefer-stateless-function
 export class Calculator extends Component {
   render() {
     return (
       <table>
         <tr>
-          <td colSpan="4" className="right"><input type="digit" placeholder="0" /></td>
+          <td colSpan="4" className="right">
+            <input type="digit" placeholder="0" />
+          </td>
         </tr>
-        <tr>
-          <td>AC</td>
-          <td>+/-</td>
-          <td>%</td>
-          <td className="right">&#247;</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td className="right">&#215;</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-          <td className="right">&#8722;</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td className="right">&#43;</td>
-        </tr>
-        <tr>
-          <td colSpan="2">0</td>
-          <td>.</td>
-          <td className="right">=</td>
-        </tr>
+        {content.map((row) => (
+          // eslint-disable-next-line react/jsx-key
+          <tr>
+            {row.map((item, i) => {
+              if (i === row.length - 1) {
+                return <td className="right">{item}</td>;
+              }
+              if (row.length === 3 && i === 0) {
+                return (
+                  <td colSpan="2">{item}</td>
+                );
+              }
+              return (
+              // eslint-disable-next-line react/jsx-key
+                <td>{item}</td>
+              );
+            })}
+          </tr>
+        ))}
       </table>
     );
   }
