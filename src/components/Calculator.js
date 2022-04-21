a54f040b0c0ec3792850743b0acb0aac035c72b2
Damola Adediran <tylher123@gmail.com>
2022-04-21 12:06:05 +0100

    start refactoring code

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 
@@ -11,48 +11,39 @@ const content = [
 ];
 
 // eslint-disable-next-line react/prefer-stateless-function
-export class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { obj: {} };
-    this.handleClick = this.handleClick.bind(this);
-  }
+export const Calculator = () => {
+  const [obj, setObj] = useState({});
 
-  handleClick(e) {
-    const { obj } = this.state;
-    this.setState({ obj: calculate(obj, e.target.textContent) });
-  }
-
-  render() {
-    const { obj } = this.state;
-    const { next, total } = obj;
-    return (
-      <div className="column">
-        <div className="row">
-          <input type="digit" placeholder="0" value={next || total} />
-        </div>
-        {content.map((row) => (
-          // eslint-disable-next-line react/jsx-key
-          <div className="row">
-            {row.map((item, i) => {
-              if (i === row.length - 1) {
-                return <button type="button" onClick={this.handleClick} className="right">{item}</button>;
-              }
-              if (row.length === 3 && i === 0) {
-                return (
-                  <button type="button" onClick={this.handleClick} className="w-2">{item}</button>
-                );
-              }
-              return (
-              // eslint-disable-next-line react/jsx-key
-                <button type="button" onClick={this.handleClick}>{item}</button>
-              );
-            })}
-          </div>
-        ))}
+  const handleClick = (e) => {
+    setObj(calculate(obj, e.target.value));
+  };
+  const { next, total } = obj;
+  return (
+    <div className="column">
+      <div className="row">
+        <input type="digit" placeholder="0" value={next || total} />
       </div>
-    );
-  }
-}
+      {content.map((row) => (
+        // eslint-disable-next-line react/jsx-key
+        <div className="row">
+          {row.map((item, i) => {
+            if (i === row.length - 1) {
+              return <button type="button" onClick={() => handleClick()} className="right">{item}</button>;
+            }
+            if (row.length === 3 && i === 0) {
+              return (
+                <button type="button" onClick={() => handleClick()} className="w-2">{item}</button>
+              );
+            }
+            return (
+            // eslint-disable-next-line react/jsx-key
+              <button type="button" onClick={() => handleClick()}>{item}</button>
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Calculator;
